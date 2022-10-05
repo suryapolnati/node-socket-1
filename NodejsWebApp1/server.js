98084efe3deda77744ece8e7ade767974acb341c
suryapolnati <59924040+suryapolnati@users.noreply.github.com>
2022-10-05 12:07:55 +0530

    adding websocket code

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -1,8 +1,38 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+// JavaScript source code
+const http = require('http');
+const server = require('websocket').server;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World 1\n');
-}).listen(port);
+const httpServer = http.createServer(() => {
+
+});
+httpServer.listen(1337, () => {
+    console.log('Server listening at port 1337');
+});
+
+const wsServer = new server({
+    httpServer,
+
+});
+
+let clients = [];
+
+const peersByCode = {};
+
+wsServer.on('request', request => {
+    const connection = request.accept();
+    const id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+
+    connection.on('message', message => {
+        const { code } = JSON.parse(message.utf8Data);
+        if (!peersByCode[code]) {
+            peersByCode[code] = [{ connection, id }];
+        } else if (!peersByCode[code].find(peer => peer.id === id)) {
+            peersByCode[code].push({ connection, id });
+        }
+
+        const peer = peersByCode[code].find(peer => peer.id !== id)
+        if (peer) {
+            peer.connection.send(message.utf8Data);
+        }
+    });
+});
