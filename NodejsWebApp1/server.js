3b94eb98f3c5698acc36ff65e6b047712cb7fa95
suryapolnati <59924040+suryapolnati@users.noreply.github.com>
2022-10-05 18:26:27 +0530

    express+cors+socketio

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -1,9 +1,28 @@
-const { Server } = require("socket.io");
+const express = require('express');
+const app = express();
+const port = 1337;
+let http = require("http").Server(app);
 
-const io = new Server();
-
-io.on("connection", (socket) => {
-    io.emit('connected', 'client connected');
+const io = require("socket.io")(http, {
+    cors: {
+        origin: "http://localhost:4200",
+        methods: ["GET", "POST"]
+    }
 });
 
-io.listen(1337);
+app.set("port",port);
+app.use(cors());
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', '*');
+
+    next();
+});
+
+io.on("connection", function (socket) {
+   
+  
+});
+
+const server = http.listen(port, function () {
+    console.log("listening on *:1337");
+});
